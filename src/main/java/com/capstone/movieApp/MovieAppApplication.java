5502ad9887275c01d5bfd3369ec11d08cdbcfaf4
vang1325 <fe3850su@go.minnstate.edu>
2022-11-18 11:32:05 -0600

    added configuration to homepage

--- src/main/java/com/capstone/movieApp/MovieAppApplication.java
+++ src/main/java/com/capstone/movieApp/MovieAppApplication.java
@@ -2,9 +2,19 @@ package com.capstone.movieApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+//@Controller
 public class  MovieAppApplication {
+
+//	@RequestMapping("/")
+//	@ResponseBody
+//	String home() {
+//		return ("./login.html");
+//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieAppApplication.class, args);
